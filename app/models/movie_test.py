673965a87bc97c297ee096ccdd7a2f5f586d400f
Jefferson Gakuya <jeffersongakuya@gmail.com>
2021-02-17 11:15:01 +0300

    Added request file for getting the object from the API

---
+++ app/models/movie_test.py
@@ -0,0 +1,22 @@
+import unittest
+from models import movie
+Movie = movie.Movie
+
+class MovieTest(unittest.TestCase):
+    '''
+    Test Class to test the behavior of the Movie class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that wil run before every Test
+        '''
+        self.new_movie = Movie(1234, 'Python Must Be Crazy', 'A thrilling new Python Series', 'https://image.tmd.org/t/p/w500/khsjha27hbs', 8.5,129993)
+
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_movie, Movie)
+        
+        
+if __name__ == '__main__':
+    unittest.main()
