700671ebde077c17ecde83e7655e2c1000820fdc
Jefferson Gakuya <jeffersongakuya@gmail.com>
2021-02-18 10:12:35 +0300

    search bar fixed able to display data

--- app/request.py
+++ app/request.py
@@ -1,11 +1,12 @@
 from app import app
 import urllib.request, json
 from .models import movie
+from app.instance.config import MOVIE_API_KEY
 
 Movie = movie.Movie
 
 # Getting api key
-api_key = app.config['MOVIE_API_KEY']
+api_key = MOVIE_API_KEY
 
 # Getting the movie base url
 base_url = app.config["MOVIE_API_BASE_URL"]
@@ -76,3 +77,18 @@ def get_movie(id):
             movie_object = Movie(id, title, overview, poster, vote_average, vote_count)
 
     return movie_object
+
+def search_movie(movie_name):
+    search_movie_url = 'https://api.themoviedb.org/3/search/movie?api_key={}&query={}'.format(api_key, movie_name)
+
+    with urllib.request.urlopen(search_movie_url) as url:
+        search_movie_data = url.read()
+        search_movie_response = json.loads(search_movie_data)
+
+        search_movie_results = None
+
+        if search_movie_response['results']:
+            search_movie_list = search_movie_response['results']
+            search_movie_results = process_results(search_movie_list)
+
+    return search_movie_results
--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-# from app.instance.config import MOVIE_API_KEY
+from app.instance.config import MOVIE_API_KEY
 
 
 class Config:
@@ -6,6 +6,7 @@ class Config:
     General configuration parent class
     '''
     MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
+    MOVIE_API_KEY 
 
 class ProdConfig(Config):
     '''
@@ -15,7 +16,8 @@ class ProdConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
 
-    MOVIE_API_KEY = '7a3b260a2b4a7805cc684ef8de37cf00'
+    # MOVIE_API_KEY = '7a3b260a2b4a7805cc684ef8de37cf00'
+    pass
 
 class DevConfig(Config):
     '''
